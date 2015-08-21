97a03ff8dd2e525099274d1d2056acb66140cb87
Joe Labi <mwbetrg@gmail.com>
2015-08-21 13:06:55 +0000

    Improved twitter

--- twitter-client.py
+++ twitter-client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #Created : Thu 06 Aug 2015 02:15:25 PM MYT
-#Last Modified : Fri 21 Aug 2015 12:59:57 PM UTC
+#Last Modified : Fri 21 Aug 2015 01:05:32 PM UTC
 
 import os
 import sys
@@ -25,3 +25,22 @@ t = Twitter(auth=OAuth(ACCESS_TOKEN,
 #t.statuses.update(status="Testing another twitter client")
 
 #print t.search.tweets(q="#najib")
+
+# Send a direct message
+#t.direct_messages.new(
+#user="awangjangok",
+#text="Nok gi sembahyang doh ning!")
+
+
+# Send images along with your tweets:
+# - first just read images from the web or from files the regular way:
+#with open("example.png", "rb") as imagefile:
+#imagedata = imagefile.read()
+## - then upload medias one by one on Twitter's dedicated server
+## and collect each one's id:
+#t_up = Twitter(domain='upload.twitter.com',
+#auth=OAuth(token, token_key, con_secret, con_secret_key))
+#id_img1 = t_up.media.upload(media=imagedata)["media_id_string"]
+#id_img2 = t_up.media.upload(media=imagedata)["media_id_string"]
+## - finally send your tweet with the list of media ids:
+#t.statuses.update(status="PTT ★", media_ids=",".join([id_img1, id_img2]))
